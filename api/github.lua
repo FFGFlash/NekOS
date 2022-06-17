c13f6da79195e62f763bbddfea4478118e8f4ada
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2022-06-16 20:31:46 -0500

    Update github.lua

--- api/github.lua
+++ api/github.lua
@@ -43,7 +43,7 @@ function Github:download(user, repo, dpath, rpath, branch)
       for str in res:gmatch('"path":"([^\"]+)"') do table.insert(fPath, str) end
       for str in res:gmatch('"name":"([^\"]+)"') do table.insert(fName, str) end
     end
-    for i,data in pairs(fTypes) do
+    for i,data in pairs(fType) do
       local path = dpath.."/"..repo.."/"
       if data == "file" then
         cPath = http.get("https://raw.github.com/"..user.."/"..repo.."/"..branch.."/"..fPath[i])
