08848055728d1c9cdc20db15cedcc338692b1329
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2022-10-14 05:55:56 -0500

    Update term.lua

--- Api/term.lua
+++ Api/term.lua
@@ -13,4 +13,21 @@ function term.writeCentered(t, w, h)
   term.write(t)
 end
 
+function term.table(t, w, s)
+  local x, y, W = term.getCursorPos(), term.getWidth()
+  w, s = w or W, s or 0
+  local R, C = #t, 0
+  for r = 1, R, 1 do C = math.max(C, #t[r]) end
+  local cw, ch = w / C, 1 + s
+  for r = 1, R, 1 do
+    local Y = y + (r - 1) * ch
+    for c = 1, C, 1 do
+      term.setCursorPos(x + (c - 1) * cw, Y)
+      if r == 1 then term.writeCentered(t[r][c], cw, 1)
+      else term.write(t[r][c])
+      end
+    end
+  end
+end
+
 return term
