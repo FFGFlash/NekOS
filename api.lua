8c1f3dc6a6d8d2f49404d3cfefbd236392d69f4f
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2022-10-13 21:43:26 -0500

    Update api.lua

--- api.lua
+++ api.lua
@@ -116,7 +116,7 @@ function Api:__call(order, completion)
   }
 
   function api:__call(...)
-    return self, self:constructor(...)
+    return self:constructor(...) or self
   end
 
   function api:printUsage()
